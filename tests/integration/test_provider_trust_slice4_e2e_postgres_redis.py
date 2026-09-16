72ea0eb9082e64fc9bae07981af06ba34ad40a2e
Soham Gajanan Sadegaonkar <adt23socb1137@students.mituniversity.edu.in>
2026-09-17 00:26:47 +0530

    test(provider): qualify trust root at feature schema head

--- tests/integration/test_provider_trust_slice4_e2e_postgres_redis.py
+++ tests/integration/test_provider_trust_slice4_e2e_postgres_redis.py
@@ -98,7 +98,7 @@ pytestmark = [
     pytest.mark.asyncio,
 ]
 
-HEAD = "20260916_clinical_access_sessions"
+HEAD = "20260916_patient_external_record_import"
 _USER_AGENT = "Nexa-Slice4-Qual-Agent/1.0"
 _CLIENT_IP = "127.0.0.1"
 _DB_NAME = "nexa_qual_slice4_e2e"
