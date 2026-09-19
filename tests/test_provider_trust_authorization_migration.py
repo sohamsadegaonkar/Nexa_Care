7d7b5340dd72ec676d4a05fa4ef3465492f6ddb0
Soham Gajanan Sadegaonkar <adt23socb1137@students.mituniversity.edu.in>
2026-09-19 18:20:19 +0530

    test(task0): update trust authorization migration head

--- tests/test_provider_trust_authorization_migration.py
+++ tests/test_provider_trust_authorization_migration.py
@@ -15,7 +15,8 @@ CLINICAL_ACCESS_REVISION = "20260916_clinical_access_sessions"
 TREATMENT_SESSION_REVISION = "20260917_treatment_session_operations"
 PATIENT_EXTERNAL_RECORD_REVISION = "20260916_patient_external_record_import"
 CANONICAL_ENCOUNTER_REVISION = "20260918_canonical_encounter"
-HEAD_REVISION = "20260918_treatment_vitals_encounter"
+TREATMENT_VITALS_REVISION = "20260918_treatment_vitals_encounter"
+HEAD_REVISION = "20260919_prescriber_eligibility"
 
 
 def test_trust_authorization_migration_is_single_head_and_forward_only() -> None:
